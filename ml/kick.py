0290d7eedbe9592854ad6ba664f3cec5ef42514d
mottai <konakona191@gmail.com>
2021-08-11 05:08:16 +0000

    full test

--- ml/kick.py
+++ ml/kick.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # url = 'http://127.0.0.1'
-url = 'http://34.146.72.59:10080/'
+url = 'http://35.187.223.154:10080/'
 payload = {"file_name": "cat.jpg"}
 
 res = requests.post(url, json=json.dumps(payload))
